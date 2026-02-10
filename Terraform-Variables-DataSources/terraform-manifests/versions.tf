43c5cdc2568eafb745de6441705d9cce161a138f
Yathendraindra <yathendra1997@gmail.com>
2026-02-10 12:18:09 +0530

    Section4 Learnings

---
+++ Terraform-Variables-DataSources/terraform-manifests/versions.tf
@@ -0,0 +1,14 @@
+# Terraform block
+terraform {
+  required_version = "~> 1.8"
+  required_providers {
+    aws = {
+       source = "hashicorp/aws"
+       version = "~> 6.0"
+    }
+  }
+}
+# Provider Block
+provider "aws" {
+  region = var.aws_region
+}
